f172e230b4f15e43011f9dbf3620e457276bac9e
phillipahereza <pahereza@gmail.com>
2019-12-15 14:50:53 +0300

    added more keywords to the langauge return, if, else, true and false

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -25,6 +25,10 @@ if (5 < 10) {
 } else {
 return false;
 }
+
+10 == 10;
+
+10 != 9;
 `
 
 	tests := []struct {
@@ -101,6 +105,16 @@ return false;
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 
 		{token.EOF, ""},
 	}
